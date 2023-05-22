3277d9621467d8c4f1fbc09e8b41fac60a9655f1
=Andrei <balasca.andrei@gmail.com>
2023-05-22 18:29:39 +0300

    created fetchcountries.js started on rest

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,12 @@
-function fetchCountries(name) {
-    name.preventDefault();
+const propreties = '?fields=name,capital,population,flags,languages';
+
+const url = 'https://restcountries.com/v3.1/name/';
+
+export const fetchCountries = name => {
+  return fetch(`${url}${name}${propreties}`).then(response => {
+    if (response.status === 404) {
+      alert('country does not exist');
+    }
+    return response.json();
+  });
 };
