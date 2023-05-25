0637caa7f3129bd4ecd9ab584a9a007a01222f2a
=Andrei <balasca.andrei@gmail.com>
2023-05-25 12:50:01 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,8 @@ const propreties = '?fields=name,capital,population,flags,languages';
 const url = 'https://restcountries.com/v3.1/name/';
 
 export const fetchCountries = name => {
-  return fetch(`${url}${name}${propreties}`).then(response => {
+  return fetch(`${url}${name}${propreties}`)
+    .then(response => {
     if (response.status === 404) {
       alert('country does not exist');
     }
